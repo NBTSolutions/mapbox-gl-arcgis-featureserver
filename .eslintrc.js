f38ca23d629904d4e235833c337d61633cdf1e31
Lucas McCain <lucas@vetrofibermap.com>
2021-09-02 19:30:46 -0400

    rollup + eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-  extends: ['airbnb', 'prettier'],
-  plugins: ['prettier'],
+  extends: ["airbnb", "prettier"],
+  plugins: ["prettier"],
   rules: {
-    'prettier/prettier': ['error'],
-    'no-param-reassign': [
-      'error',
+    "prettier/prettier": ["error"],
+    "no-param-reassign": [
+      "error",
       {
         props: false,
       },
     ],
-    'no-underscore-dangle': 0,
-    'import/prefer-default-export': 0,
-    'no-unused-vars': [
+    "no-underscore-dangle": 0,
+    "import/prefer-default-export": 0,
+    "no-unused-vars": [
       2,
       {
         ignoreRestSiblings: true,
-        argsIgnorePattern: '^__',
-        varsIgnorePattern: '^__',
+        argsIgnorePattern: "^__",
+        varsIgnorePattern: "^__",
       },
     ],
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
   },
 };
